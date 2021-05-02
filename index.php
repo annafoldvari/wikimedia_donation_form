46b826e8cf05ca481961982e552a9d02b079d307
anna_f <annafoldvari@gmx.co.uk>
2021-05-02 14:56:43 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  include("inc/functions.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Wikimedia Foundation Donation Form</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+  </head>
+  <body>
+    <main role="main">
+      <?php echo test(); ?>
+    </main>
+  </body>
+</head>  
+
+
+	
---
+++ tests/Tests.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Tests extends TestCase
+{
+    public function testTest()
+    {
+        $this->assertIsString(test());
+    }
+}
